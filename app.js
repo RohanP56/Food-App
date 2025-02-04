28afd9f46ff3aafa4b93e77b811d0e507a53f535
Rohan <devx.rohan@gmail.com>
2025-02-04 07:06:36 +0530

    Set up initial project structure with Express, Mongoose, and user authentication

---
+++ app.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const userModel = require("./models/userModel");
+
+//middleware
+app.use(express.json());
+app.listen(3000);
+app.use(cookieParser());
+
+/*let users = [
+  {
+    id: 1,
+    name: "Rohan",
+  },
+  {
+    id: 2,
+    name: "Jasbir",
+  },
+  {
+    id: 3,
+    name: "Kartik",
+  },
+];*/
+
+// mini application
+const userRouter = express.Router();
+const authRouter = express.Router();
+
+//base route, router to use
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
+
+userRouter
+  .route("/")
+  .get(getUsers)
+  .post(postUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+//Route for cookies
+userRouter.route("/getcookies").get(getCookies);
+userRouter.route("/setcookies").get(setCookies);
+
+userRouter.route("/:id").get(getUserById);
+
+authRouter.route("/signup").get(getSignUp).post(postSignUp);
+
+async function getUsers(req, res) {
+  let allUsers = await userModel.find(); //find all users
+  //let user = await userModel.findOne({name:"Rohan"}); //find one user
+  res.json({ message: "List of all users", data: allUsers });
+}
+
+function getUserById(req, res) {
+  //console.log(req.params.id);
+  let paramId = req.params.id;
+  let obj = {};
+  for (let i = 0; i < users.length; i++) {
+    if (users[i]["id"] == paramId) {
+      obj = users[i];
+    }
+  }
+  res.json({
+    obj,
+  });
+}
+
+function postUser(req, res) {
+  users = req.body;
+  req.json({
+    message: "Data recived Successfully",
+    user: req.body,
+  });
+}
+
+function updateUser(req, res) {
+  //console.log(req.body);
+  let dataToBeUpdate = req.body;
+  for (key in dataToBeUpdate) {
+    users[key] = dataToBeUpdate[key];
+  }
+  res.json({
+    message: "Data Updated Successfully",
+  });
+}
+
+function deleteUser(req, res) {
+  users = {};
+  res.json({
+    message: "Data Deleted Successfully",
+  });
+}
+
+function getSignUp(req, res) {
+  res.sendFile("/public/index.html", { root: __dirname });
+}
+
+function postSignUp(req, res) {
+  let obj = req.body; // data will come as an object
+  console.log("Object Recived from frontend: ", obj);
+  res.json({
+    message: "user signed up",
+    data: obj,
+  });
+}
+
+//cookies function implementation
+function setCookies(req, res) {
+  //res.setHeader("Set-Cookie", "isLoggedIn=true");  // here we set cookie manually
+  res.cookie("isLoggedIn", true);  // set cookie using cookie parser
+  res.send("Cookies set successfully");
+}
+
+function getCookies(req, res) {
+  //console.log(req.cookies);
+ /* res.json({
+    message: "Cookies fetched successfully",
+  });*/
+}
---
+++ models/userModel.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const emailValidator = require("email-validator");
+
+//database connection
+const db_link =
+  "mongodb+srv://admin:30Rm0SDcjdyMJP1y@cluster0.zs2x3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose
+  .connect(db_link)
+  .then(function (db) {
+    //console.log(db);
+    console.log("Database connected");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+//user schema
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: function () {
+      return emailValidator.validate(this.email);
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+    minLength: 8,
+    validate: function () {
+      return this.confirmPassword === this.password;
+    },
+    //message: "Password and confirm password do not match",
+  },
+});
+
+//we don't want to save the confirmPassword in the database
+userSchema.pre("save", function () {
+  this.confirmPassword = undefined; // if we do anything undefineed then it will not be stored in DB
+});
+
+//generating salt and hashing the password
+userSchema.pre("save", async function () {
+  let salt = await bcrypt.genSalt();
+  let hashedString = await bcrypt.hash(this.password, salt);
+  //console.log(hashedString);
+  this.password = hashedString;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+const userModel = mongoose.model("userModel", userSchema);
+module.exports = userModel;
+
+
+
+
+/*Mongoose hooks pre, post , remove
+pre hook
+userSchema.pre("save",function() {
+  console.log("Before save in DB", this);
+});
+
+//post hook
+userSchema.post("save",function(doc) {
+  console.log("After save in DB", doc);
+});
+
+*/
